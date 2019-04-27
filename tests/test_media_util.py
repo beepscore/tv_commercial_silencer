4cac5373a96a191aff480998ffe5418d6cf92804
beepscore <support@beepscore.com>
2019-04-27 15:04:51 -0700

    Add test_media_durations_second_dict_keys_dont_end_with_mp3

--- tests/test_media_util.py
+++ tests/test_media_util.py
@@ -18,6 +18,17 @@ class TestMediaUtil(unittest.TestCase):
         actual = media_util.duration_seconds_from_media_file(filename2)
         self.assertAlmostEqual(actual, 248.036, places=2)
 
+    def test_media_durations_second_dict_keys_dont_end_with_mp3(self):
+        indirname = './src/pydejavu/mp3'
+        media_dict_filename = './data/media_durations_test.json'
+        # write media dictionary
+        media_util.write_media_file_durations(indirname, media_dict_filename)
+        media_duration_dict = media_util.media_durations_second_dict(media_dict_filename)
+        self.assertIsNone(media_duration_dict.get('Brad-Sucks--Total-Breakdown.mp3'))
+        self.assertIsNotNone(media_duration_dict.get('Brad-Sucks--Total-Breakdown'))
+        # clean up
+        os.remove(media_dict_filename)
+
     def test_media_durations_second_dict(self):
         indirname = './src/pydejavu/mp3'
         media_dict_filename = './data/media_durations_test.json'
