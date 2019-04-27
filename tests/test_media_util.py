bb12e2e7f555a56cc679f92a7394a054ffc37c1c
beepscore <support@beepscore.com>
2019-04-27 10:18:29 -0700

    Add test_media_durations_second_dict

--- tests/test_media_util.py
+++ tests/test_media_util.py
@@ -17,3 +17,16 @@ class TestMediaUtil(unittest.TestCase):
         actual = media_util.duration_seconds_from_media_file(filename2)
         self.assertAlmostEqual(actual, 248.036, places=2)
 
+    def test_media_durations_second_dict(self):
+        indirname = './src/pydejavu/mp3'
+        media_dict_filename = './data/media_durations_test.json'
+        # write media dictionary
+        media_util.write_media_file_durations(indirname, media_dict_filename)
+        media_duration_dict = media_util.media_durations_second_dict(media_dict_filename)
+        self.assertEqual(len(media_duration_dict), 5)
+        self.assertAlmostEqual(media_duration_dict.get('Brad-Sucks--Total-Breakdown.mp3'),
+                               138.997, places=2)
+        self.assertAlmostEqual(media_duration_dict.get('The-Lights-Galaxia--While-She-Sleeps.mp3'),
+                               248.036, places=2)
+
+
