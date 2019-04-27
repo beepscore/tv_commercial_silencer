a7ce99255f3e4b8d431830369fd2fdad6c4193d5
beepscore <support@beepscore.com>
2019-04-27 09:59:38 -0700

    Add assertion

--- tests/test_media_util.py
+++ tests/test_media_util.py
@@ -8,7 +8,12 @@ class TestMediaUtil(unittest.TestCase):
 
     def test_duration_seconds_from_media_file(self):
         # use media files from package pydejavu
-        filename = './src/pydejavu/mp3/Brad-Sucks--Total-Breakdown.mp3'
-        actual = media_util.duration_seconds_from_media_file(filename)
+
+        filename1 = './src/pydejavu/mp3/Brad-Sucks--Total-Breakdown.mp3'
+        actual = media_util.duration_seconds_from_media_file(filename1)
         self.assertAlmostEqual(actual, 138.997, places=2)
+
+        filename2 = './src/pydejavu/mp3/The-Lights-Galaxia--While-She-Sleeps.mp3'
+        actual = media_util.duration_seconds_from_media_file(filename2)
+        self.assertAlmostEqual(actual, 248.036, places=2)
 
